e3d07e5103da57cff200fb13bc12d0d87333512c
Bogdan <bogdan@supersimple.dev>
2025-03-28 22:29:53 +0100

    First commit

---
+++ script.js
@@ -0,0 +1,16 @@
+var weddingDate = new Date("May 11, 2025 00:00:00").getTime();
+
+function updateCountdown() {
+    var now = new Date().getTime();
+    var timeLeft = weddingDate - now;
+
+    var days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
+
+    document.getElementById("countdown").innerHTML = days + " dana " + (hours + 1) + " sati " + minutes + " minuta " + seconds + " sekundi";
+}
+
+updateCountdown();
+setInterval(updateCountdown, 1000);
