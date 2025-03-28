ffd973c938f294e279d4225585fcf619c35b3a89
Bogdan Radojičić <160786688+6ogdann@users.noreply.github.com>
2025-03-28 22:34:51 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 var weddingDate = new Date("May 11, 2025 00:00:00").getTime();
 
 function updateCountdown() {
@@ -14,21 +13,3 @@ function updateCountdown() {
 }
 
 updateCountdown();
-=======
-var weddingDate = new Date("May 11, 2025 00:00:00").getTime();
-
-function updateCountdown() {
-    var now = new Date().getTime();
-    var timeLeft = weddingDate - now;
-
-    var days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
-    var hours = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    var minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
-    var seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
-
-    document.getElementById("countdown").innerHTML = days + " dana " + hours + " sati " + minutes + " minuta " + seconds + " sekundi";
-}
-
-updateCountdown();
->>>>>>> 1d03f9793794c58cec5f97a573b12f7839e89a40
-setInterval(updateCountdown, 1000);
